0ce70aa56231b86d05aeb65018c52d8a961af853
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2021-07-12 11:26:04 +0530

    updated

--- js1/objects/index.js
+++ js1/objects/index.js
@@ -99,6 +99,23 @@ let student2= createStudentmarks1(82,80,75);
 console.log(student1, student2);
 
 //constructor function
+/* 
+Instead of returning an object, we're going to use a different approach 
+to initialize an object. In JavaScript, we have a keyword called this. 
+And this is a reference to the object that is executing this piece 
+of code.
+
+For now, just imagine this references an empty object.
+Now you know what with dot notation, we can access
+properties of an object, we can read a property, or we can set a property, so
+on this new empty object, we want to add a property
+called physics, and we set that to this physics argument that
+you receive as parameter. Similarly do this for chemistry and maths.
+So in JavaScript our objects are dynamic.
+Once we create them we can always add additional properties or
+methods to them. So here we are adding a new property but
+empty object. 
+*/
 console.log("we use pascal notation for constructor function");
 function StudentMarks(physics, chemistry, maths){
     this.physics = physics;
@@ -114,3 +131,56 @@ function StudentMarks(physics, chemistry, maths){
 let stu1 = new StudentMarks(75,80,82);
 console.log(stu1.greeting());
 console.log(stu1.aggregate());
+/*
+when we use new operator here, 3 things happen. This new operator 
+creates an empty JavaScript object. Something like this -> let stu1 ={};
+
+Next, it will set this keyword to point to this new empty object stu1. 
+So in this code we have access to this empty object(stu1), and we set all 
+the properties that uses this keyword to point object stu1.
+
+Finally, this new operator will return this new object from this function.
+Previously we added return in factory functions but new operator will 
+take care about this.
+
+*/
+
+/*
+Objects in js is dynamic which means once you create them you can always 
+add new properties or methods, or remove existing ones.
+
+*/
+const myName = {
+    firstname: 'Nithin'
+};
+console.log(myName);
+myName.secondname = 'Shanmugam';
+console.log(myName);
+delete myName.firstname;
+console.log(myName);
+/*
+we have used the const keyword, to define a new constant. However, you 
+can see I have modified the circle object of either the property, and 
+then remove that property. So what kind of constant is that?
+
+Well, when we use constant here, that means we cannot reassign this 
+variable, so this variable is more accurately a constant. So we cannot
+reset circle to a new object. If we do that like, circle = {}; 
+we get this error. Assignment to constant variable. So we cannot reassign 
+this constant, but we can always change the circle object by adding
+or removing properties.
+*/
+
+
+/* 
+property constructor references the function that was used to construct or 
+create that object. 
+*/
+console.log(
+    "student1.constructor(created using factory functions) : \n",
+    student1.constructor
+);
+console.log(
+    "stu1.constructor(created using constructor functions) : \n",
+    stu1.constructor
+);
